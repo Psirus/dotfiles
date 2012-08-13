72075ed24466570c1bcaffb71f879e7ecdfd4c14
Christoph Pohl <schnasni@lavabit.com>
2012-08-13 20:15:08 +0200

    Fullscreen Layout added

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,18 @@
 import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Config.Xfce
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageDocks
+
+myLayout = avoidStruts (
+    Tall 1 (3/100) (1/2) |||
+    Mirror (Tall 1 (3/100) (1/2))) |||
+    noBorders (fullscreenFull Full)
 
 main = xmonad $ xfceConfig
-	{ terminal = "xfce4-terminal"
+	{ terminal = "xfce4-terminal",
+    layoutHook = myLayout
 	} `additionalKeys`
 	[ ((mod1Mask, xK_F4), kill)
 	]
