d75dbdac4243e61931360d951ba6c033eb76d901
Christoph Pohl <schnasni@lavabit.com>
2012-08-13 22:47:32 +0200

    change colour

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,18 +5,16 @@ import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
 import XMonad.Config.Desktop
 
-import qualified Data.Map as M
-
 myLayout = avoidStruts (
     Tall 1 (3/100) (1/2) |||
     Mirror (Tall 1 (3/100) (1/2))) |||
     noBorders (fullscreenFull Full)
 
 main = xmonad $ desktopConfig
-	{ terminal = "xfce4-terminal",
-    layoutHook = myLayout
+	{ terminal = "xfce4-terminal"
+    , focusedBorderColor = "#79a142"
+    , layoutHook = myLayout
 	} `additionalKeys`
 	[ ((mod1Mask, xK_F4), kill)
-    , ((mod1Mask, xK_b), spawn "dmenu_run")
     , ((mod1Mask .|. shiftMask, xK_q), spawn "xfce4-session-logout")
     ]
