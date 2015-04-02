a16845d761a4797f24b77b3b3da7c911feb76df4
Christoph Pohl <christoph.pohl1@mailbox.tu-dresden.de>
2015-04-02 12:12:13 +0200

    Display skype indicator only when skype is running

--- message_indicator.py
+++ message_indicator.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8
 import Skype4Py
+import subprocess
+import re
 
-skype = Skype4Py.Skype()
-skype.Attach()
+skypeRunning = False
+s = subprocess.Popen(["ps","axw"],stdout=subprocess.PIPE)
+for x in s.stdout:
+    if re.search("skype", x):
+        skypeRunning = True
 
-if len(skype.MissedMessages) == 0:
-    print "^fg()0"
-else:
-    print "^fg(#FF0000)" + str(len(skype.MissedMessages))
+if skypeRunning:
+    skype = Skype4Py.Skype()
+    skype.Attach()
+    icon = "^fg(#00adef)਀ "
+    if len(skype.MissedMessages) == 0:
+        print icon + "^fg()0"
+    else:
+        print icon + "^fg(#FF0000)" + str(len(skype.MissedMessages))
